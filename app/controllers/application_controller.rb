9c518b82f486a07d8748dc062f802f2ac6015082
snowflake <polytronix1@gmail.com>
2020-11-03 10:23:10 -0600

    valip ip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
                "120.33.232.194",   #PE Fujian
                "68.203.130.1",    #PI Remote
                "127.0.0.1","::1"        #localhost
-               "54.196.166.54"]     #ping pcms ip address
+               "52.6.217.20]     #ping pcms ip address
 
   protect_from_forgery
   before_action :check_auth, except: [:new, :create]
