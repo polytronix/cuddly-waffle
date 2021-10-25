37af909fd2e6441d9adf7413d524d9e3aa4445c5
snowflake <polytronix1@gmail.com>
2021-10-25 13:57:11 -0500

    syntehic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_action :check_auth, except: [:new, :create]
   #before_action :check_auth
-  before_action :check_ip - remove ip check
+  before_action :check_ip  # remove ip check
   before_action :set_raven_user_context, if: :current_user
   around_action :set_tenant_time_zone, if: :current_tenant
 
