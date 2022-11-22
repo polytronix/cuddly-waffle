5498e6959ba13de538c2f6d39caeda1bdbd3df46
Snowflake <polytronix1@gmail.com>
2022-11-22 15:29:08 -0600

    9 to 16

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -3,12 +3,12 @@ class Tenant
     'pi' => { name: "PI",
               time_zone: "Central Time (US & Canada)", 
               area_divisor: 144.0, 
-              small_area_cutoff: 1296, ##2304 (<16 to <9)
+              small_area_cutoff: 2304 ## 1296, ##2304 (<16 to <9)
               yield_multiplier: 1 },
     'pe' => { name: "PE",
               time_zone: "Beijing", 
               area_divisor: 1000000.0, 
-              small_area_cutoff: 836000.0, ##1500000 (<16 to <9)
+              small_area_cutoff: 1500000 ##836000.0, ##1500000 (<16 to <9)
               yield_multiplier: 10.76 }
   }
 
